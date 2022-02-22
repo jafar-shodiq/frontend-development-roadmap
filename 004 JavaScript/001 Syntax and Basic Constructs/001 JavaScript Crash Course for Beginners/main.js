8b06d7cb29fcf054a20f0ec204a3072d87e0686c
Jafar Shodiq <j.shodiq2312@gmail.com>
2022-02-22 14:23:21 +0700

    first commit

--- 004 JavaScript/001 Syntax and Basic Constructs/001 JavaScript Crash Course for Beginners/main.js
+++ 004 JavaScript/001 Syntax and Basic Constructs/001 JavaScript Crash Course for Beginners/main.js
@@ -59,3 +59,59 @@ console.log('');
 console.log('');
 console.log('');
 
+function addNums(num1, num2 = 5) {
+    return num1 + num2;
+}
+console.log(addNums(6));
+
+const addNums2 = (num1, num2 = 5) => {
+    return num1 + num2;
+}
+console.log(addNums2(6));
+
+const addNums3 = num1 => num1 + 5;
+console.log(addNums(6));
+
+console.log('');
+console.log('');
+console.log('');
+
+// constructor function
+// function Person(firstName, lastName, dob) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.dob = new Date(dob);
+// }
+
+// Person.prototype.getMyBirthYear = function() {
+//     return this.dob.getFullYear();
+// }
+
+// Person.prototype.getMyFullName = function() {
+//     return `${this.firstName} ${this.lastName}`;
+// }
+
+// class -> does the same thing as the constructor function
+class Person {
+    constructor(firstName, lastName, dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = new Date(dob);
+    }
+
+    getMyBirthYear() {
+        return this.dob.getFullYear();
+    }
+
+    getMyFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+// instantiate object
+const person1 = new Person('Jafar', 'Shodiq', '12-23-1997');
+console.log(person1);
+console.log(person1.dob.getFullYear());
+console.log(person1.getMyBirthYear());
+console.log(person1.getMyFullName());
+
